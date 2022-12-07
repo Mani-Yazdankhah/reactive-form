b66bb6f45c0639d07c019d3bfc5442cb37ee4364
Mani-Yazdankhah <mani.yazdankhah@accolitedigital.com>
2022-12-07 12:15:58 +0000

    finished version

--- src/app/modules/vendor/active-vendor/active-vendor.component.ts
+++ src/app/modules/vendor/active-vendor/active-vendor.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-active-vendor',
@@ -16,7 +16,27 @@ export class ActiveVendorComponent implements OnInit {
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       bio: ['', Validators.required],
-      domain: ''
+      domain: ['', Validators.required]
     })
   }
+
+  get isNameNotFilled() { return this.venderForm.get('name')?.errors?.['required'] && this.venderForm.get('name')?.touched }
+  get isEmailNotFilled() { return this.venderForm.get('email')?.errors?.['required'] && this.venderForm.get('email')?.touched }
+  get isEmailInvalid() { return this.venderForm.get('email')?.errors?.['email'] }
+  get isBioNotFilled() { return this.venderForm.get('bio')?.errors?.['required'] && this.venderForm.get('bio')?.touched }
+  get isDomainNotFilled() { return this.venderForm.get('domain')?.errors?.['required'] && this.venderForm.get('domain')?.touched }
+
+  onSubmit() {
+    let info = document.getElementById("info");
+    if (info != undefined) {
+      info.innerHTML = `<div>
+      <h1>Object should be print here</h1>
+      name: ${this.venderForm.get("name")?.value} <br>
+      email: ${this.venderForm.get("email")?.value}<br>
+      bio: ${this.venderForm.get("bio")?.value}<br>
+      domain: ${this.venderForm.get("domain")?.value}<br>
+      </div>`
+    }
+  }
+
 }
